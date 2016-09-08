b5d643a4bee789aa5fffa91acdd6a0be9251e40e
Paul Mun <paulmun818@gmail.com>
2016-09-08 15:41:12 -0700

    dojoSurvey assignment has been updated with validations for empty fields

--- python/flaskFundamentals/dojoSurvey/server.py
+++ python/flaskFundamentals/dojoSurvey/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 app = Flask(__name__)
+app.secret_key='yup'
 @app.route('/')
 def index():
   return render_template("index.html")
@@ -9,5 +10,14 @@ def result():
 	location=request.form['location']
 	language=request.form['language']
 	comment=request.form['comment']
+	if len(name) < 1:
+		flash("Name cannot be empty!")
+		return redirect('/')
+	if len(comment) > 120:
+		flash("Please keep your comments short!")
+		return redirect('/')
+	elif len(comment) < 1:
+		flash("Maybe not so optional...")
+		return redirect('/')
 	return render_template("result.html", name=name, location=location, language=language, comment=comment)
 app.run(debug=True)
