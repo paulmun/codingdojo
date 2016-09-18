c1ca9512607dda4d650fac4f8b72b69bfc9b6604
Paul Mun <paulmun818@gmail.com>
2016-09-18 16:12:11 -0700

    Friendships assignment completed.

--- python/django/wallErd/main/apps/wallErd/models.py
+++ python/django/wallErd/main/apps/wallErd/models.py
@@ -4,22 +4,23 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	first_name = models.CharField(max_length = 255)
-	last_name = models.CharField(max_length = 255)
-	email = models.CharField(max_length = 255)
-	password = models.CharField(max_length = 255)
-	created_at = models.DateTimeField(auto_now_add = True)
-	updated_at = models.DateTimeField(auto_now = True)
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	email = models.EmailField()
+	password = models.CharField(max_length=200)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
 
 class Message(models.Model):
-	message = models.TextField(max_length = 1000)
-	user_id = models.ForeignKey(User)
-	created_at = models.DateTimeField(auto_now_add = True)
-	updated_at = models.DateTimeField(auto_now = True)
+	message = models.TextField()
+	user = models.ForeignKey(User)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-	comment = models.TextField(max_length = 1000)
-	message_id = models.ForeignKey(Message)
-	user_id = models.ForeignKey(User)
-	created_at = models.DateTimeField(auto_now_add = True)
-	updated_at = models.DateTimeField(auto_now = True)
+	comment = models.TextField()
+	user = models.ForeignKey(User)
+	message = models.ForeignKey(Message)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
